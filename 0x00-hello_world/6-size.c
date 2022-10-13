a5ea6433c3f9ed49724104bb014f9d619e3c715d
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-10-13 09:26:52 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return - Always 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,4 +12,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
