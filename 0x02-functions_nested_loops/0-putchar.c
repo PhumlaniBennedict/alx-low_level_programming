ab26744de3950624792c9df44f28955caf1c83bc
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-10-19 03:20:55 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - prints _pitchar and a new line
- * Return: 0 as always
+ * main - Print _putchar
+ * Return: Always 0
  */
 int main(void)
 {
@@ -14,5 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
+}
