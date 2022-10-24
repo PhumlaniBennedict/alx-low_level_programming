4adc2198020a321ea9ead521ab9d9e3f9a12cc40
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-10-24 11:09:20 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the vqlue of n to 98
+ * reset_to_98 - update the value of n to 98
  * @n: input
  * Return: n
  */
-int reset_to_98(int *n)
+void reset_to_98(int *n)
+
 {
 	*n = 98;
 }
