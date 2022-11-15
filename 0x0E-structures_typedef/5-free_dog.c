2020c2d3248224abdaec2a7860656883479eba62
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-11-15 13:28:52 -0800

    task5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib>
+#include "dog.h"
+
+/**
+ * free_dog - free dogs
+ *
+ * @d: dogs free
+ *
+ * Return: Void, always
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(p);
+	}
+}
+
