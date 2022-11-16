c9abb353285fe97af04f351dddd5a96b680b238c
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-11-16 11:59:49 -0800

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <function_pointers.h>
+
+/**
+ * print_name - print name
+ *
+ * @name: name print
+ *
+ * @f: pinter
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
