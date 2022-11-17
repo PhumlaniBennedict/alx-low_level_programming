daef827dd40bc371d96db5f77f94c87029b3d81d
PhumlaniBennedict <phumlanebebnedict@gmail.com>
2022-11-17 11:43:41 -0800

    task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - adds functions
+ * @a: input one
+ * @b: input two
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts functions
+ * @a: input one
+ * @b: input two
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies input
+ * @a: input one
+ * @b: input two
+ * Return: product
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - input devision remainder
+ * @a: input one
+ * @b: input two
+ * Return: devision remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
